1fc614439b60ded47fec2dfb850501065c8036db
Ahmad Zaidan <ahmad.zaidan8a8@gmail.com>
2024-05-30 11:57:27 +0200

    Build the color mode switch.

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,12 +1,13 @@
-import { HStack, Text } from "@chakra-ui/react"
+import { HStack } from "@chakra-ui/react"
 import { FaReact } from "react-icons/fa6";
+import ColorModeSwitch from "./ColorModeSwitch";
 
 
 const Navbar = () => {
   return (
-    <HStack>
+    <HStack justifyContent='space-between' padding='10px'>
         <FaReact size={60} color="dodgerblue" />
-        <Text>Navbar</Text>
+        <ColorModeSwitch />
     </HStack>
   )
 }

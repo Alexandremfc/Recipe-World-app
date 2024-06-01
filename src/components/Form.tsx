d677c6dcf1b24b569bf42ff6f585d2a9f24a1758
Ahmad Zaidan <ahmad.zaidan8a8@gmail.com>
2024-06-01 15:36:50 +0200

    simplify the form logic using react hook forms.

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -8,51 +8,29 @@ import {
   Button,
 } from "@chakra-ui/react";
 import { FormEvent, useState } from "react";
+import { FieldValues, useForm } from "react-hook-form";
 
 const Form = () => {
-  const [user, setUser] = useState({
-    name: "",
-    email: "",
-    password: "",
-  });
+  const { register, handleSubmit } = useForm();
 
-  const handleSubmit = (event: FormEvent) => {
-    event.preventDefault();
-    console.log(user);
+  const onSubmit = (data: FieldValues) => {
+    console.log(data);
   };
+
   return (
-    <form onSubmit={handleSubmit}>
+    <form onSubmit={handleSubmit(onSubmit)}>
       <FormControl>
         <Box mb="3">
           <FormLabel htmlFor="name">Name</FormLabel>
-          <Input
-            onChange={(event) => setUser({ ...user, name: event.target.value })}
-            value={user.name}
-            id="name"
-            type="text"
-          />
+          <Input {...register("name")} id="name" type="text" />
         </Box>
         <Box mb="3">
           <FormLabel htmlFor="email">Email address</FormLabel>
-          <Input
-            value={user.email}
-            onChange={(event) =>
-              setUser({ ...user, email: event.target.value })
-            }
-            id="email"
-            type="email"
-          />
+          <Input {...register("email")} id="email" type="email" />
         </Box>
         <Box mb="3">
           <FormLabel htmlFor="password">Password</FormLabel>
-          <Input
-            onChange={(event) =>
-              setUser({ ...user, password: event.target.value })
-            }
-            value={user.password}
-            id="password"
-            type="password"
-          />
+          <Input {...register("password")} id="password" type="password" />
         </Box>
         <Button mt={4} type="submit" colorScheme="teal">
           Signup

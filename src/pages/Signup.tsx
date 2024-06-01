495acd151486c7cb244fc96505b5c6198f1831fa
Ahmad Zaidan <ahmad.zaidan8a8@gmail.com>
2024-06-01 23:55:13 +0200

    implement the signup page.

--- src/pages/Signup.tsx
+++ src/pages/Signup.tsx
@@ -1,7 +1,24 @@
+import { Button, Container, Flex, Heading, Text } from "@chakra-ui/react";
 import Form from "../components/Form";
+import Navbar from "../components/Navbar";
+import { Link } from "react-router-dom";
 
 const Signup = () => {
-  return <Form />;
+  return (
+    <>
+      <Navbar />
+      <Container>
+        <Heading size='3xl' mb="15px" textAlign="center">
+          Signup
+        </Heading >
+        <Text textAlign="center" mb="10px">
+          Welcome to Recipe World . please signup or login in order to share
+          your meals.{" "}
+        </Text>
+        <Form />
+      </Container>
+    </>
+  );
 };
 
 export default Signup;

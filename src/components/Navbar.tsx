ee0c18912d466369aa9bacc66fe76fabe0b6f741
Ahmad Zaidan <ahmad.zaidan8a8@gmail.com>
2024-06-04 00:26:49 +0200

    adding a button to create a new Recipe.

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,11 +1,13 @@
-import { HStack, Image } from "@chakra-ui/react";
+import { Button, HStack, Image } from "@chakra-ui/react";
 import ColorModeSwitch from "./ColorModeSwitch";
 import logo from "../assets/logo.png";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
     <HStack justifyContent="space-between" padding="10px">
       <Image src={logo} boxSize='100px'  />
+      <Button><Link to="/addRecipe">Create a new Recipe</Link></Button>
       <ColorModeSwitch />
     </HStack>
   );
--- src/pages/routes.tsx
+++ src/pages/routes.tsx
@@ -3,12 +3,14 @@ import Signup from "./Signup";
 import Login from "./Login";
 import App from "../App";
 import RecipeDetails from "../components/RecipeDetails";
+import RecipeForm from "../components/RecipeForm";
 
 const router = createBrowserRouter([
   { path: "/", element: <App /> },
   { path: "/signup", element: <Signup /> },
   { path: "/login", element: <Login /> },
   { path: "/recipes/:id", element: <RecipeDetails /> },
+  { path: "/addRecipe", element: <RecipeForm /> },
 ]);
 
 export default router;

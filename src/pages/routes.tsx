c25da23e841572a699ae197be01fe64a1149344a
Ahmad Zaidan <ahmad.zaidan8a8@gmail.com>
2024-06-03 12:56:24 +0200

    implement the recipe details component and route.

--- src/pages/routes.tsx
+++ src/pages/routes.tsx
@@ -2,11 +2,13 @@ import { createBrowserRouter } from "react-router-dom";
 import Signup from "./Signup";
 import Login from "./Login";
 import App from "../App";
+import RecipeDetails from "../components/RecipeDetails";
 
 const router = createBrowserRouter([
   { path: "/", element: <App /> },
   { path: "/signup", element: <Signup /> },
   { path: "/login", element: <Login /> },
+  { path: "/recipes/:id", element: <RecipeDetails /> },
 ]);
 
 export default router;
